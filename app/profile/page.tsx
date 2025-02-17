c023f24ccee0fb1ca21cf42dd76627578afec399
sipalingoding <arsenalsalmanfpl@gmail.com>
2025-02-17 21:51:00 +0700

    slicing page profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,24 @@
+import { SubmitButton } from "@/components/form/Buttons";
+import FormInput from "@/components/form/FormInput";
+import Navbar from "@/components/navbar/Navbar";
 import React from "react";
 
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <Navbar />
+      <div className="w-screen bg-gray-300 h-52 mt-12"></div>
+      <div className="w-60 bg-gray-200 h-60 rounded-full mb-8 -mt-32"></div>
+      <div className="flex flex-col space-y-4 w-80">
+        <FormInput name="label1" type="text" placeholder="Label" />
+        <FormInput name="label1" type="text" placeholder="Label" />
+        <FormInput name="label1" type="text" placeholder="Label" />
+        <FormInput name="label1" type="text" placeholder="Label" />
+        <FormInput name="label1" type="text" placeholder="Label" />
+      </div>
+      <SubmitButton text="Button" withIcon className="mt-8" />
+    </div>
+  );
 }
 
 export default ProfilePage;
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { FaPlus } from "react-icons/fa6";
 function Navbar() {
   return (
     <nav className="border-b">
-      <div className="container absolute top-1 left-1/2 transform -translate-x-1/2 flex justify-between items-center py-4 gap-2 border-b-2">
+      <div className="container absolute top-1 left-1/2 transform -translate-x-1/2 flex justify-between items-center py-4 gap-2">
         <div className="flex gap-2 items-center bg-white rounded-3xl p-3">
           <FaPlus />
           <span className="text-sm">Donasi</span>
