7652697b4774b2e92ab7db33134b315aa28b9ec5
sipalingoding <arsenalsalmanfpl@gmail.com>
2025-02-20 08:01:42 +0700

    add best donator slicing on donator page

--- app/donasi/page.tsx
+++ app/donasi/page.tsx
@@ -12,13 +12,34 @@ function DonasiPage() {
             parantos mantosan arurang ngamumule bahasa sareng budaya Sunda ku
             cara manjangkeun umur ieu website
           </span>
-          <div className="flex flex-col gap-[11px] items-center">
-            <div className="flex gap-6 items-center">
-              <span>1st</span>
-              
+          <div className="flex flex-col gap-[11px] items-end justify-center">
+            <div className="flex gap-8 items-center">
+              <span className="font-semibold">1st</span>
+              <span className="flex gap-4 items-center">
+                <div className="w-8 h-8 rounded-full bg-gray-400 flex justify-center items-center">
+                  <span className="text-white text-xs">MK</span>
+                </div>
+                <span>NAMI DONATUR</span>
+              </span>
             </div>
-            <div></div>
-            <div></div>
+            <div className="flex gap-8 items-center">
+              <span className="font-semibold">2nd</span>
+              <span className="flex gap-4 items-center">
+                <div className="w-8 h-8 rounded-full bg-gray-400 flex justify-center items-center">
+                  <span className="text-white text-xs">MK</span>
+                </div>
+                <span>NAMI DONATUR</span>
+              </span>
+            </div>
+            <div className="flex gap-8 items-center">
+              <span className="font-semibold">3rd</span>
+              <span className="flex gap-4 items-center">
+                <div className="w-8 h-8 rounded-full bg-gray-400 flex justify-center items-center">
+                  <span className="text-white text-xs">MK</span>
+                </div>
+                <span>NAMI DONATUR</span>
+              </span>
+            </div>
           </div>
         </div>
         <div className="border-2 w-full border-gray-200 h-[740px] rounded-sm"></div>
